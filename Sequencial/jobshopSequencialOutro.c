fb566018eee762122951180ed67936218a4c04db
Gonçalo Maciel <goncalomaciel2250@gmail.com>
2024-05-01 15:00:16 +0100

    yah

--- Sequencial/jobshopSequencialOutro.c
+++ Sequencial/jobshopSequencialOutro.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-#define NUM_JOBS 3
-#define NUM_MACHINES 3
+#define NUM_JOBS 100
+#define NUM_MACHINES 100
 
 typedef struct {
     int machine;
@@ -25,13 +27,23 @@ int main() {
     int machine_time[NUM_MACHINES] = {0};
     Schedule schedule[NUM_JOBS * NUM_MACHINES];
     int current_operation[NUM_JOBS] = {0};
+    char file_name[50];
+    char output_file_name[50];
+
+    printf("Type the file name to read\n");
+    scanf("%s", file_name);
+
+    char openPath[100] = "../ft/";
+    strncat(openPath, file_name, strlen(file_name));
 
     // Read job data from file
-    FILE *file = fopen("../ft/ft03.jss", "r");
+    FILE *file = fopen(openPath, "r");
     if (file == NULL) {
         printf("Could not open file\n");
         return 1;
     }
+
+    clock_t start = clock();
 
     int num_jobs, num_machines;
     fscanf(file, "%d %d", &num_jobs, &num_machines);
@@ -43,7 +55,7 @@ int main() {
     }
 
     fclose(file);
-
+    
     // Initialize machine availability times
     int machine_availability[NUM_MACHINES] = {0};
     // Initialize job end times
@@ -69,11 +81,6 @@ int main() {
             job_end_times[i] = schedule[i * num_machines + j].end_time;
         }
     }
-
-    // Print the schedule
-    for (int i = 0; i < num_jobs * num_machines; i++) {
-        printf("Job %d starts at time %d on machine %d\n", schedule[i].job, schedule[i].start_time, schedule[i].machine);
-    }
     // Initialize makespan
     int makespan = 0;
 
@@ -84,7 +91,35 @@ int main() {
         }
     }
 
+    clock_t end = clock();
+    double time_taken = ((double)end - start) / CLOCKS_PER_SEC;
+    
+    printf("Enter the name of the file to write the results to: ");
+    scanf("%s", output_file_name);
+
+    char resultsPath[100] = "../Resultados/";
+    strncat(resultsPath, output_file_name, strlen(output_file_name));
+
+    // Open the output file
+    FILE *output_file = fopen(resultsPath, "w");
+    if (output_file == NULL) {
+        printf("Could not open or create file %s\n", output_file_name);
+        return 1;
+    }
+
+    // Print the schedule
+    for (int i = 0; i < num_jobs * num_machines; i++) {
+        printf("Job %d - Machine %d - Start %d - End %d\n", schedule[i].job, schedule[i].machine, schedule[i].start_time, schedule[i].end_time);
+        fprintf(output_file, "Job %d - Machine %d - Start %d - End %d\n", schedule[i].job, schedule[i].machine, schedule[i].start_time, schedule[i].end_time);
+
+    }
+
     printf("The makespan is %d\n", makespan);
+    printf("Time taken: %f seconds\n", time_taken);
+    fprintf(output_file, "The makespan is %d\n", makespan);
+    fprintf(output_file, "Time taken: %f seconds\n", time_taken);
+    
+    fclose(output_file);
 
     return 0;
 }
